addbf2684b0c4c4531a538b41d4f368e5f784266
matt-h487 <111523930+matt-h487@users.noreply.github.com>
2023-06-08 18:22:22 -0500

    still an error in getting type modifiers

--- Pokemon.py
+++ Pokemon.py
@@ -60,6 +60,7 @@ class Pokemon_Move:
         self.priority = 0
         self.accuracy = 0
         self.type = ''
+        self.damage_class = ''
         self.get_data()
     def get_data(self):
         url = f'https://pokeapi.co/api/v2/move/{self.name.lower()}/'
@@ -70,20 +71,52 @@ class Pokemon_Move:
         self.accuracy = move_data['accuracy']
         self.priority = move_data['priority']
         self.type = move_data['type']['name']
+        self.damage_class = move_data['damage_class']['name']
+        self.type_effectiveness = self.get_type_effectiveness(self.type)
+
+    def get_type_effectiveness(self, type):
+            url = f'https://pokeapi.co/api/v2/type/{type}/'
+            response1 = requests.get(url)
+            if response1.status_code == 200:
+                type_data = response1.json()
+                weakness_data = type_data.get('damage_relations')
+
+                if weakness_data is None:
+                    print(f"No damage_relations found for type '{type}'")
+                    return {}
+
+                type_effectiveness = {
+                    'double_damage_from': [],
+                    'double_damage_to': [],
+                    'half_damage_from': [],
+                    'half_damage_to': [],
+                    'no_damage_from': [],
+                    'no_damage_to': []
+                }
+
+                for relation_name, relation_types in weakness_data.items():
+                    for relation_type in relation_types:
+                        type_effectiveness[relation_name].append(relation_type['name'])
+
+                return type_effectiveness
+            else:
+                print("Error getting Type Information")
+                return {}
 
     def display(self):
-        print(f'{self.name}: {self.pp} pp, {self.power} power, {self.accuracy} accuracy, {self.type}')
+        print(f'{self.name.capitalize()}: {self.pp} pp, {self.power} power, {self.accuracy} accuracy, {self.type}')
 
 class Pokemon_Battle:
     def __init__(self, Pokemon1, Pokemon2):
         self.Pokemon1 = Pokemon1
         self.Pokemon2 = Pokemon2
         self.turn = False
+        self.battle()
 
     def battle(self):
         while self.Pokemon1.hp > 0 and self.Pokemon2.hp > 0:
-            print(f'{self.Pokemon1.name.capitalize} has {self.Pokemon1.hp} hp.')
-            print(f'{self.Pokemon2.name.capitalize} has {self.Pokemon2.hp} hp.')
+            print(f'{self.Pokemon1.name.capitalize()} has {self.Pokemon1.hp} hp.')
+            print(f'{self.Pokemon2.name.capitalize()} has {self.Pokemon2.hp} hp.')
 
             if self.Pokemon1.speed > self.Pokemon2.speed:
                 self.perform_turn(self.Pokemon1, self.Pokemon2)
@@ -91,45 +124,65 @@ class Pokemon_Battle:
                     self.perform_turn(self.Pokemon2, self.Pokemon1)
             elif self.Pokemon2.speed > self.Pokemon1.speed:
                 self.perform_turn(self.Pokemon2, self.Pokemon1)
-                if self.Pokemon1.hp > 0:  # Check if the battle has already ended
+                if self.Pokemon1.hp > 0:
                     self.perform_turn(self.Pokemon1, self.Pokemon2)
             else:
-                # If both Pokemon have the same speed, randomly determine which one moves first
                 first_pokemon = random.choice([self.Pokemon1, self.Pokemon2])
                 second_pokemon = self.Pokemon2 if first_pokemon == self.Pokemon1 else self.Pokemon1
                 self.perform_turn(first_pokemon, second_pokemon)
                 if second_pokemon.hp > 0:  # Check if the battle has already ended
                     self.perform_turn(second_pokemon, first_pokemon)
 
+        if self.Pokemon1.hp <= 0:
+            print(f'{self.Pokemon1.name.capitalize()} has fainted.')
+        if self.Pokemon2.hp <= 0:
+            print(f'{self.Pokemon2.name.capitalize()} has fainted.')
+
     def perform_turn(self, attacking_Pokemon, defending_Pokemon):
         damage = 0
-        STAB = False
+        chance = random.randint(1, 100)
+
 
         for move in attacking_Pokemon.moves:
             move.display()
         user_choice = input('Choose a move: ').lower()
         for move in attacking_Pokemon.moves:
             if user_choice == move.name:
-                if move.type == attacking_Pokemon.type:
+                if chance <= move.accuracy:
+                    modifier = self.get_effectiveness(move, defending_Pokemon)
+                    if move.damage_class == 'physical':
+                        damage = (((2 * attacking_Pokemon.level / 5 + 2) * move.power * (attacking_Pokemon.attack / defending_Pokemon.defense) / 50) + 2) * modifier
+                    elif move.damage_class == 'special':
+                        damage = (((2 * attacking_Pokemon.level / 5 + 2) * move.power * (attacking_Pokemon.spattack / defending_Pokemon.spdefense) / 50) + 2) * modifier
+                    if move.type in attacking_Pokemon.types:
+                        damage = damage * 1.5
+                else:
+                    print(f'{move.name.capitalize()} missed!')
+        defending_Pokemon.hp -= damage
 
 
 
 
 
-    def get_TypeDamage(self, type):
-            url = f'https://pokeapi.co/api/v2/type/{type}/'
-            response1 = requests.get(url)
-            if response1.status_code == 200:
-                type_data = response1.json()
-                weakness_data = type_data['damage_relations']['double_damage_from']
-                type_names = [t["name"] for t in weakness_data]
-                for name in type_names:
-                    print(name)
-            else:
-                print("Error getting Type Information")
+    def get_effectiveness(self, move, defending_pokemon):
+        damage_modifier = 1.0
+        types = defending_pokemon.types
+        type_effectiveness = move.type_effectiveness
+
+        for defending_type in types:
+            modifiers = type_effectiveness.get(defending_type, [])
+            for modifier in modifiers:
+                if modifier == 'double_damage_to':
+                    damage_modifier *= 2
+                elif modifier == 'half_damage_to':
+                    damage_modifier *= 0.5
+                elif modifier == 'no_damage_to':
+                    damage_modifier *= 0
+        return damage_modifier
 
 
-blastoise = Pokemon('blastoise',  5, [Pokemon_Move('crunch'), Pokemon_Move('tackle'), Pokemon_Move('water gun'), Pokemon_Move('hydro pump')])
-Charizard = Pokemon('charizard', 10, [Pokemon_Move('fly'), Pokemon_Move('flamethrower'), Pokemon_Move('wing attack'), Pokemon_Move('bite')])
 
+blastoise = Pokemon('blastoise',  50, [Pokemon_Move('crunch'), Pokemon_Move('tackle'), Pokemon_Move('water-gun'), Pokemon_Move('hydro-pump')])
+Charizard = Pokemon('charizard', 50, [Pokemon_Move('fly'), Pokemon_Move('flamethrower'), Pokemon_Move('wing-attack'), Pokemon_Move('bite')])
+battle = Pokemon_Battle(Charizard, blastoise)
 
